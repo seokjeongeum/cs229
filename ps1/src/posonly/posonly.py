81317d6529bf78b7c8cb30f9121ec97fb62520ac
seokjeongeum <jeongeumseok910@gmail.com>
2024-01-19 14:00:26 +0900

    1.2

--- ps1/src/posonly/posonly.py
+++ ps1/src/posonly/posonly.py
@@ -35,37 +35,45 @@ def main(train_path, valid_path, test_path, save_path):
     # *** START CODE HERE ***
     # Part (a): Train and test on true labels
     # Make sure to save predicted probabilities to output_path_true using np.savetxt()
-    x_train, t_train = util.load_dataset(
+    x_train_a, t_train_a = util.load_dataset(
         train_path,
-        label_col='t',
-        add_intercept=True
+        't',
+        True,
     )
     x_test, t_test = util.load_dataset(
         test_path,
-        label_col='t',
-        add_intercept=True
+        't',
+        True,
     )
-    clf = LogisticRegression()
-    clf.fit(x_train, t_train)
-    np.savetxt(output_path_true, clf.predict(x_test))
-    util.plot(x_test, t_test, clf.theta, f'{os.path.splitext(output_path_true)[0]}.png')
+    clf_a = LogisticRegression()
+    clf_a.fit(x_train_a, t_train_a)
+    np.savetxt(output_path_true, clf_a.predict(x_test))
+    util.plot(x_test, t_test, clf_a.theta, f'{os.path.splitext(output_path_true)[0]}.png')
     # Part (b): Train on y-labels and test on true labels
     # Make sure to save predicted probabilities to output_path_naive using np.savetxt()
-    x_train, y_train = util.load_dataset(
+    x_train_b, y_train_b = util.load_dataset(
         train_path,
-        add_intercept=True
+        add_intercept=True,
     )
-    x_test, t_test = util.load_dataset(
-        test_path,
-        label_col='t',
-        add_intercept=True
-    )
-    clf = LogisticRegression()
-    clf.fit(x_train, y_train)
-    np.savetxt(output_path_naive, clf.predict(x_test))
-    util.plot(x_test, t_test, clf.theta, f'{os.path.splitext(output_path_naive)[0]}.png')
+    clf_b = LogisticRegression()
+    clf_b.fit(x_train_b, y_train_b)
+    np.savetxt(output_path_naive, clf_b.predict(x_test))
+    util.plot(x_test, t_test, clf_b.theta, f'{os.path.splitext(output_path_naive)[0]}.png')
     # Part (f): Apply correction factor using validation set and test on true labels
     # Plot and use np.savetxt to save outputs to output_path_adjusted
+    x_valid, y_valid = util.load_dataset(
+        valid_path,
+        add_intercept=True,
+    )
+    alpha = clf_b.predict(x_valid[y_valid == 1]).mean()
+    np.savetxt(output_path_adjusted, clf_b.predict(x_test) / alpha)
+    util.plot(
+        x_test,
+        t_test,
+        clf_b.theta,
+        f'{os.path.splitext(output_path_adjusted)[0]}.png',
+        alpha,
+    )
     # *** END CODER HERE
 
 
